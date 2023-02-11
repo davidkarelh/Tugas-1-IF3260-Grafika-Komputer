3a990cbd2f6d7b8f0ca7217ccb8d0b58d1917fa7
davidkarelh <kareldavid9@gmail.com>
2023-02-11 12:08:25 +0700

    Add square (create, translation, dilation)

---
+++ src/square.js
@@ -0,0 +1,270 @@
+/* eslint no-console:0 consistent-return:0 */
+"use strict";
+
+var canvas, gl, index = 0;
+
+function main() {
+  canvas = document.querySelector("canvas");
+
+  // Get A WebGL context
+  gl = WebGLUtils.setupWebGL( canvas );
+
+  if (!gl) {
+    alert( "WebGL isn't available" );
+    return;
+  }
+
+  let createShapeRadio = document.getElementById("createShapeRadio");
+  let selectShapeRadio = document.getElementById("selectShapeRadio");
+  let dilationSlider = document.getElementById("dilation-slider");
+
+  var arrayOfShapes = [];
+  var shape_index = 0;
+  var mouseDown = false;
+  var mouseDownPosition = null;
+
+  var clickMode = 0;
+  var selected_shape = null;
+  var selected_shape_center = null;
+
+  var is_moving_shape = false;
+
+  var t1, t2, t3, t4;
+  var first = true;
+  
+  // Get the strings for our GLSL shaders
+  var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+  var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
+
+  // create GLSL shaders, upload the GLSL source, compile the shaders
+  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+  // Link the two shaders into a program
+  var program = createProgram(gl, vertexShader, fragmentShader);
+
+  // look up where the vertex data needs to go.
+  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+  // Create a buffer and put three 2d clip space points in it
+  var positionBuffer = gl.createBuffer();
+
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  gl.bufferData(gl.ARRAY_BUFFER, 10240, gl.STATIC_DRAW);
+
+  // code above this line is initialization code.
+  // code below this line is rendering code.
+
+  gl.canvas.width = (5 / 12) * window.innerWidth;
+  gl.canvas.height = (5 / 12) * window.innerWidth;
+
+  // Tell WebGL how to convert from clip space to pixels
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  // Clear the canvas
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Tell it to use our program (pair of shaders)
+  gl.useProgram(program);
+
+  // Turn on the attribute
+  gl.enableVertexAttribArray(positionAttributeLocation);
+
+  // Bind the position buffer.
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  var size = 2;          // 2 components per iteration
+  var type = gl.FLOAT;   // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+
+  var oldDilationSliderValue = 1;
+  dilationSlider.addEventListener("change", (e) => {
+    // console.log(dilationSlider.value);
+    if (selected_shape != null) {
+      let dilateValue = (dilationSlider.value / oldDilationSliderValue);
+      gl.bufferSubData(gl.ARRAY_BUFFER, 8*selected_shape.index, new Float32Array([selected_shape.t1[0] * dilateValue, selected_shape.t1[1] * dilateValue]));
+      gl.bufferSubData(gl.ARRAY_BUFFER, 8*(selected_shape.index+1), new Float32Array([selected_shape.t3[0] * dilateValue, selected_shape.t3[1] * dilateValue]));
+      gl.bufferSubData(gl.ARRAY_BUFFER, 8*(selected_shape.index+2), new Float32Array([selected_shape.t2[0] * dilateValue, selected_shape.t2[1] * dilateValue]));
+      gl.bufferSubData(gl.ARRAY_BUFFER, 8*(selected_shape.index+3), new Float32Array([selected_shape.t4[0] * dilateValue, selected_shape.t4[1] * dilateValue]));
+
+      arrayOfShapes[selected_shape.shape_index].t1 = [selected_shape.t1[0] * dilateValue, selected_shape.t1[1] * dilateValue];
+      arrayOfShapes[selected_shape.shape_index].t3 = [selected_shape.t3[0] * dilateValue, selected_shape.t3[1] * dilateValue];
+      arrayOfShapes[selected_shape.shape_index].t2 = [selected_shape.t2[0] * dilateValue, selected_shape.t2[1] * dilateValue];
+      arrayOfShapes[selected_shape.shape_index].t4 = [selected_shape.t4[0] * dilateValue, selected_shape.t4[1] * dilateValue];
+    }
+    oldDilationSliderValue = dilationSlider.value;
+  })
+
+  createShapeRadio.addEventListener("click", (e) => {
+    clickMode = 0;
+    selected_shape = null;
+    selected_shape_center = null;
+    dilationSlider.disabled = true;
+    dilationSlider.value = 1;
+    oldDilationSliderValue = 1;
+  });
+
+  selectShapeRadio.addEventListener("click", (e) => {
+    clickMode = 1;
+  });
+
+  canvas.addEventListener("mousedown", function(event) {
+    mouseDown = true;
+    mouseDownPosition = getPositionInCanvas(event);
+    // console.log("mouedown");
+  });
+  
+  canvas.addEventListener("mouseup", function(event) {
+    mouseDown = false;
+    // console.log("mouseup");
+    if (is_moving_shape) {
+      var position = getPositionInCanvas(event);
+      is_moving_shape = false;
+      var old_difference = { x: selected_shape_center[0] - mouseDownPosition.x, y: selected_shape_center[1] - mouseDownPosition.y };
+      var new_difference = { x: selected_shape_center[0] - position.x, y: selected_shape_center[1] - position.y };
+      var correction = { x: old_difference.x - new_difference.x, y: old_difference.y - new_difference.y };
+
+      arrayOfShapes[selected_shape.shape_index].t1 = [selected_shape.t1[0] + correction.x, selected_shape.t1[1] + correction.y];
+      arrayOfShapes[selected_shape.shape_index].t3 = [selected_shape.t3[0] + correction.x, selected_shape.t3[1] + correction.y];
+      arrayOfShapes[selected_shape.shape_index].t2 = [selected_shape.t2[0] + correction.x, selected_shape.t2[1] + correction.y];
+      arrayOfShapes[selected_shape.shape_index].t4 = [selected_shape.t4[0] + correction.x, selected_shape.t4[1] + correction.y];
+    }
+  });
+
+  canvas.addEventListener("mouseleave", function(event) {
+    mouseDown = false;
+  });
+
+  canvas.addEventListener("click", function(event) {
+    gl.bindBuffer( gl.ARRAY_BUFFER, positionBuffer);
+    var position = getPositionInCanvas(event);
+    if (clickMode == 0) {
+      if (first) {
+        first = false;     
+        t1 = [position.x, position.y]
+  
+      } else {
+        first = true;
+        var abs_x = Math.abs(t1[0] - position.x);
+        var abs_y = Math.abs(t1[1] - position.y);
+        
+        if (abs_x < abs_y) {
+          t2 = [position.x, (position.y < t1[1]) ?  (t1[1] - abs_x): (t1[1] + abs_x)];
+        } else {
+          t2 = [(position.x < t1[0]) ?  (t1[0] - abs_y): (t1[0] + abs_y), position.y];
+        }
+  
+        t3 = [t1[0], t2[1]];
+        t4 = [t2[0], t1[1]];
+  
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, new Float32Array(t1));
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+1), new Float32Array(t3));
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+2), new Float32Array(t2));
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+3), new Float32Array(t4));
+        index += 4;
+  
+        arrayOfShapes.push({
+          name: "square",
+          shape_index,
+          t1,
+          t2,
+          t3,
+          t4,
+          index : index - 4
+        });
+
+        shape_index += 1;
+
+        // console.log(arrayOfShapes);
+          
+        // gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
+        // var t = [0.0, 0.0, 1.0, 1.0];
+  
+        // gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-4), t);
+        // gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-3), t);
+        // gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-2), t);
+        // gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-1), t);
+
+      }
+    } else if (clickMode == 1) {
+      let shape = isInAShape(event, arrayOfShapes);
+
+      if (shape != null) {
+        selected_shape = shape;
+        selected_shape_center = getCenterOfShape(selected_shape);
+        dilationSlider.disabled = false;
+      } else {
+        selected_shape = null;
+        selected_shape_center = null;
+        dilationSlider.disabled = true;
+        dilationSlider.value = 1;
+        oldDilationSliderValue = 1;
+      }
+    }
+  });
+
+  canvas.addEventListener("mousemove", function(event) {
+    var position = getPositionInCanvas(event);
+    if (clickMode == 0) {
+      canvas.style.cursor = "pointer";
+  
+      if(!first) {
+        gl.bindBuffer( gl.ARRAY_BUFFER, positionBuffer);
+  
+        var abs_x = Math.abs(t1[0] - position.x);
+        var abs_y = Math.abs(t1[1] - position.y);
+        
+        if (abs_x < abs_y) {
+          t2 = [position.x, (position.y < t1[1]) ?  (t1[1] - abs_x): (t1[1] + abs_x)];
+        } else {
+          t2 = [(position.x < t1[0]) ?  (t1[0] - abs_y): (t1[0] + abs_y), position.y];
+        }
+  
+        t3 = [t1[0], t2[1]];
+        t4 = [t2[0], t1[1]];
+  
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, new Float32Array(t1));
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+1), new Float32Array(t3));
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+2), new Float32Array(t2));
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+3), new Float32Array(t4));
+      }
+    } else if (clickMode == 1) {
+      if (selected_shape != null && mouseDown) {
+        if (positionInShape(mouseDownPosition, selected_shape)) {
+          is_moving_shape = true;
+          var old_difference = { x: selected_shape_center[0] - mouseDownPosition.x, y: selected_shape_center[1] - mouseDownPosition.y };
+          var new_difference = { x: selected_shape_center[0] - position.x, y: selected_shape_center[1] - position.y };
+          var correction = { x: old_difference.x - new_difference.x, y: old_difference.y - new_difference.y };
+
+          gl.bufferSubData(gl.ARRAY_BUFFER, 8*selected_shape.index, new Float32Array([selected_shape.t1[0] + correction.x, selected_shape.t1[1] + correction.y]));
+          gl.bufferSubData(gl.ARRAY_BUFFER, 8*(selected_shape.index+1), new Float32Array([selected_shape.t3[0] + correction.x, selected_shape.t3[1] + correction.y]));
+          gl.bufferSubData(gl.ARRAY_BUFFER, 8*(selected_shape.index+2), new Float32Array([selected_shape.t2[0] + correction.x, selected_shape.t2[1] + correction.y]));
+          gl.bufferSubData(gl.ARRAY_BUFFER, 8*(selected_shape.index+3), new Float32Array([selected_shape.t4[0] + correction.x, selected_shape.t4[1] + correction.y]));
+
+        }
+      }
+    }
+  });
+
+  render();
+
+  function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    var drawLimit = first ? index : (index + 4);
+
+    for(var i = 0; i < drawLimit; i+=4) {
+      gl.drawArrays( gl.TRIANGLE_FAN, i, 4 );
+    }
+
+    window.requestAnimationFrame(render);
+  }
+}
+
+main();
---
+++ src/utils.js
@@ -0,0 +1,91 @@
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+  
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+function getPositionInCanvas(event) {
+    return {
+        x: (2 * event.clientX) / canvas.width - 1,
+        y: (2 * (canvas.height - event.clientY)) / canvas.height - 1,
+    }
+}
+
+function isInAShape(event, arrayOfShapes) {
+    var position = getPositionInCanvas(event);
+    
+    for (shape of arrayOfShapes) {
+        // if (position.x <= shape.bottom_right_corner[0] && position.x >= shape.upper_left_corner[0] && position.y >= shape.bottom_right_corner[1] && position.y <= shape.upper_left_corner[1]) {
+        if (positionInShape(position, shape)) {
+            return shape;
+        }
+    }
+    return null;
+}
+
+function positionInShape(position, shape) {
+    // return (position.x <= shape.bottom_right_corner[0] && position.x >= shape.upper_left_corner[0] && position.y >= shape.bottom_right_corner[1] && position.y <= shape.upper_left_corner[1]);
+    var left, right, top, bottom;
+
+    if (shape.t1[0] < shape.t2[0]) {
+        left = shape.t1[0];
+        right = shape.t2[0];
+    } else {
+        right = shape.t1[0];
+        left = shape.t2[0];
+    }
+
+    if (shape.t1[1] < shape.t2[1]) {
+        bottom = shape.t1[1];
+        top = shape.t2[1];
+    } else {
+        top = shape.t1[1];
+        bottom = shape.t2[1];
+    }
+
+    return (position.x <= right && position.x >= left && position.y >= bottom && position.y <= top);
+}
+
+function getCenterOfShape(shape) {
+    var left, right, top, bottom;
+
+    if (shape.t1[0] < shape.t2[0]) {
+        left = shape.t1[0];
+        right = shape.t2[0];
+    } else {
+        right = shape.t1[0];
+        left = shape.t2[0];
+    }
+
+    if (shape.t1[1] < shape.t2[1]) {
+        bottom = shape.t1[1];
+        top = shape.t2[1];
+    } else {
+        top = shape.t1[1];
+        bottom = shape.t2[1];
+    }
+
+    var ret = [(right + left) / 2, (top + bottom) / 2];
+    return ret;
+}
